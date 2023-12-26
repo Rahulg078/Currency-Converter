47a3d17613563eff791313a9e98caf1a9bc7fbce
RAHUL GUPTA <106857092+Rahulg078@users.noreply.github.com>
2023-12-26 17:47:38 +0530

    Added index.html

---
+++ countryConverter.js
@@ -0,0 +1,13 @@
+const countryList = {
+    AED: "AE",
+    AFN: "AF",
+    AMD: "AM",
+    GMD: "GM",
+    INR: "IN",
+    NIO: "NI",
+    NZD: "NZ",
+    OMR: "OM",
+    PEN: "PE",
+    PHP: "PH",
+    USD: "US",
+  };
